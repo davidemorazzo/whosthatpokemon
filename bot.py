c97c795e0d20ba05801d55f964e8d3294351943e
Davide Morazzo <morazzo.davide@gmail.com>
2021-06-22 17:13:43 +0200

    Initial commit no patreon

--- bot.py
+++ bot.py
@@ -26,6 +26,7 @@ if __name__ == '__main__':
         try:
             ## => CONNECT TO HEROKU POSTGRESQL
             HEROKU_DB_STRING = os.environ.get("DATABASE_URL")#.replace("postgresql://", "postgresql+psycopg2://")
+            print(HEROKU_DB_STRING)
             engine = init_database(HEROKU_DB_STRING)
         except Exception as e:
             print(e)
