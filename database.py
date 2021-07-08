90a65d79bfb113d4d870e551d55b9401b5e68282
Davide Morazzo <morazzo.davide@gmail.com>
2021-07-08 23:22:26 +0200

    Initial commit on new repository

--- database.py
+++ database.py
@@ -58,7 +58,7 @@ class botChannelIstance(Base):
 
 def init_database(url:String):
 
-    engine = create_engine(url, echo=True) #TODO: togliere echo
+    engine = create_engine(url, echo=False)
     Base.metadata.create_all(bind=engine)
     return engine
 
